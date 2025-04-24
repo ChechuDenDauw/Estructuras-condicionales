1a1e3d92f672244e10978fa4fcad497ccc10890d
Cecy Den Dauw <ceciliadendauw@gmail.com>
2025-04-24 10:50:46 -0300

    Actividad 6

--- Actividades.py
+++ Actividades.py
@@ -31,3 +31,22 @@ if 8 <= len(contrasena) <= 14:
     print("Ha ingresado una contraseña correcta")
 else:
     print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
+    # Actividad 6
+import random
+from statistics import mean, median, mode
+numeros_aleatorios = [random.randint(1, 100) for i in range(50)]
+media = mean(numeros_aleatorios)
+mediana = median(numeros_aleatorios)
+moda = mode(numeros_aleatorios)
+print("Lista de números:", numeros_aleatorios)
+print(f"Media: {media:.2f}")
+print(f"Mediana: {mediana}")
+print(f"Moda: {moda}")
+if media > mediana > moda:
+    print("Sesgo positivo (a la derecha)")
+elif media < mediana < moda:
+    print("Sesgo negativo (a la izquierda)")
+elif media == mediana == moda:
+    print("Distribución sin sesgo")
+else:
+    print("No se puede determinar un sesgo claro con los valores obtenidos")
