e0f3b2a6c6483147214a21a10e3520278911243c
Cecy Den Dauw <ceciliadendauw@gmail.com>
2025-04-24 10:47:16 -0300

    Actividad 5

--- Actividades.py
+++ Actividades.py
@@ -25,3 +25,9 @@ elif edad_categoria < 30:
     print("Adulto/a joven")
 else:
     print("Adulto/a")
+    # Actividad 5
+contrasena = input("Ingrese una contraseña (8 a 14 caracteres): ")
+if 8 <= len(contrasena) <= 14:
+    print("Ha ingresado una contraseña correcta")
+else:
+    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
