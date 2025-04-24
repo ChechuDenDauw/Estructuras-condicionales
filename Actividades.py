25b0ccfe8e2b36cf4833fcab380cfe773ffd6171
Cecy Den Dauw <ceciliadendauw@gmail.com>
2025-04-24 10:45:57 -0300

    Actividad 4

--- Actividades.py
+++ Actividades.py
@@ -15,3 +15,13 @@ if numero % 2 == 0:
     print("Ha ingresado un número par")
 else:
     print("Por favor, ingrese un número par")
+    # Actividad 4
+edad_categoria = int(input("Ingrese su edad para categorizarlo: "))
+if edad_categoria < 12:
+    print("Niño/a")
+elif edad_categoria < 18:
+    print("Adolescente")
+elif edad_categoria < 30:
+    print("Adulto/a joven")
+else:
+    print("Adulto/a")
