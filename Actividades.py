872c36edfd5324bb5be4df59302cb31b53fb2e57
Cecy Den Dauw <ceciliadendauw@gmail.com>
2025-04-24 10:45:29 -0300

    Actividad 3

--- Actividades.py
+++ Actividades.py
@@ -9,3 +9,9 @@ if nota >= 6:
     print("Aprobado")
 else:
     print("Desaprobado")
+# Actividad 3
+numero = int(input("Ingrese un número par: "))
+if numero % 2 == 0:
+    print("Ha ingresado un número par")
+else:
+    print("Por favor, ingrese un número par")
